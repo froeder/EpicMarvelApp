2c263398af6e4d2414ec0b50a652fce5152c9d7d
froeder <froeder3@gmail.com>
2019-04-27 23:07:50 -0400

    Busca resulta em lista de personagens

--- app/src/pages/Index.vue
+++ app/src/pages/Index.vue
@@ -9,27 +9,29 @@
     <br>
     <q-card
       class="text-white"
-      v-if="personagem.name"
-      style="background-color:#8E7D6B"
+      v-for="personagem in personagens"
+      :key="personagem.id"
+      style="background-color:#8E7D6B ; margin-bottom:1em"
     > 
       <q-img
-        :src="imagem"
+        :src="personagem.imagem"
       >
         <div class="absolute-bottom text-subtitle2 text-center">
-          {{personagem.name}}
+          {{personagem.nome}}
         </div>
       </q-img>
 
       <q-card-section>
-        {{descricao}}
+        {{personagem.descricao || 'Sem descrição'}}
       </q-card-section>
+      <q-separator/>
       <q-card-actions align="around">
         <q-btn flat round color="black" icon="visibility" />
         <q-btn flat round color="yellow" icon="star" />
         <q-btn flat round color="primary" icon="share" />
       </q-card-actions>
-    </q-card> 
-    
+    </q-card>
+
   </q-page>
 </template>
 
@@ -45,9 +47,8 @@ export default {
   name: 'PageIndex',
   data(){
     return{
-      personagem: [],
-      descricao: '',
-      imagem: '',
+      // personagem: [],
+      personagens: [],
       busca:''
     }
   },
@@ -71,32 +72,30 @@ export default {
         messageColor: 'black'
       })
     },
-    traduz(personagem){
-      var self = this
-      let texto = personagem.toString()
-      if(texto == ''){
-        self.descricao = 'Sem descrição'
-      } else {
-        console.log(personagem)
-        var googleTranslate = require('google-translate')('AIzaSyC_Vt7D6DAWGohSGOFmD6urdGCvcoodmts');
-        googleTranslate.translate(texto, 'pt', function(err, translation) {
-          self.descricao = translation.translatedText
-        });
-      }        
-     
+    traduz(resultado){
+      let desc = ''
+      let self = this
+      for(let i = 0 ; i < resultado.length ; i++) {
+
+      var googleTranslate = require('google-translate')('AIzaSyC_Vt7D6DAWGohSGOFmD6urdGCvcoodmts');
+        googleTranslate.translate(resultado[i].description, 'pt', function(err, translation) {
+          desc = translation.translatedText
+          self.personagens.push({
+            nome: resultado[i].name,
+            descricao: desc,
+            imagem: resultado[i].thumbnail.path+'.'+resultado[i].thumbnail.extension,
+            id: resultado[i].id
+          })
+        });            
+      }  
     },
     buscarPersonagens(nome){
       var self = this
-      self.personagem = []
-      self.descricao = ''
       this.showLoading()
       MarvelApi.getAllCharacters(nome, characters => {
         let resultado = characters.data.data.results
         if(resultado.length > 0){
-          this.traduz(characters.data.data.results[0].description)
-          self.personagem = characters.data.data.results[0]
-          self.imagem = self.personagem.thumbnail.path + '.jpg'
-
+          this.traduz(resultado)
           this.$q.loading.hide()
         } else{
           this.$q.loading.hide()
@@ -107,6 +106,7 @@ export default {
         }
         
       })
+      // console.log(self.personagens)
     }
   },
    beforeDestroy () {
